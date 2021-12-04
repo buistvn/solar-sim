e47734cb2b4d67fa0007ac8bdcd0649eeab8c32f
Steven Bui <bui.stvn@gmail.com>
2021-12-04 13:52:11 -0800

    Implement sphere function

--- Final Project/sample.cpp
+++ Final Project/sample.cpp
@@ -32,11 +32,11 @@
 //		6. The transformations to be reset
 //		7. The program to quit
 //
-//	Author:			Joe Graphics
+//	Author: Steven Bui
 
 // title of these windows:
 
-const char *WINDOWTITLE = { "OpenGL / GLUT Sample -- Joe Graphics" };
+const char *WINDOWTITLE = { "OpenGL / GLUT Sample -- Steven Bui" };
 const char *GLUITITLE   = { "User Interface Window" };
 
 // what the glui package defines as true and false:
@@ -147,6 +147,13 @@ const GLfloat Colors[ ][3] =
 	{ 0., 0., 0. },		// black
 };
 
+struct point
+{
+	float x, y, z;		// coordinates
+	float nx, ny, nz;	// surface normal
+	float s, t;		// texture coords
+};
+
 // fog parameters:
 
 const GLfloat FOGCOLOR[4] = { .0, .0, .0, 1. };
@@ -179,6 +186,8 @@ int		WhichColor;				// index into Colors[ ]
 int		WhichProjection;		// ORTHO or PERSP
 int		Xmouse, Ymouse;			// mouse values
 float	Xrot, Yrot;				// rotation angles in degrees
+int		NumLngs, NumLats;
+struct 	point *	Pts;
 
 
 // function prototypes:
@@ -216,6 +225,10 @@ short			ReadShort( FILE * );
 void			Cross(float[3], float[3], float[3]);
 float			Dot(float [3], float [3]);
 float			Unit(float [3], float [3]);
+
+inline struct point * 	PtsPointer( int, int );
+inline void 			DrawPoint( struct point * );
+void 					OsuSphere( float, int, int );
 
 
 // main program:
@@ -384,7 +397,7 @@ Display( )
 
 	// draw the current object:
 
-	glCallList( BoxList );
+	OsuSphere( 1.0, 100, 100 );
 
 #ifdef DEMO_Z_FIGHTING
 	if( DepthFightingOn != 0 )
@@ -395,31 +408,6 @@ Display( )
 		glPopMatrix( );
 	}
 #endif
-
-	// draw some gratuitous text that just rotates on top of the scene:
-
-	glDisable( GL_DEPTH_TEST );
-	glColor3f( 0., 1., 1. );
-	DoRasterString( 0., 1., 0., (char *)"Text That Moves" );
-
-	// draw some gratuitous text that is fixed on the screen:
-	//
-	// the projection matrix is reset to define a scene whose
-	// world coordinate system goes from 0-100 in each axis
-	//
-	// this is called "percent units", and is just a convenience
-	//
-	// the modelview matrix is reset to identity as we don't
-	// want to transform these coordinates
-
-	glDisable( GL_DEPTH_TEST );
-	glMatrixMode( GL_PROJECTION );
-	glLoadIdentity( );
-	gluOrtho2D( 0., 100.,     0., 100. );
-	glMatrixMode( GL_MODELVIEW );
-	glLoadIdentity( );
-	glColor3f( 1., 1., 1. );
-	DoRasterString( 5., 5., 0., (char *)"Text That Doesn't" );
 
 	// swap the double-buffered framebuffers:
 
@@ -1441,4 +1429,124 @@ Unit(float vin[3], float vout[3])
 		vout[2] = vin[2];
 	}
 	return dist;
+}
+
+inline
+struct point *
+PtsPointer( int lat, int lng )
+{
+	if( lat < 0 )	lat += (NumLats-1);
+	if( lng < 0 )	lng += (NumLngs-0);
+	if( lat > NumLats-1 )	lat -= (NumLats-1);
+	if( lng > NumLngs-1 )	lng -= (NumLngs-0);
+	return &Pts[ NumLngs*lat + lng ];
+}
+
+inline
+void
+DrawPoint( struct point *p )
+{
+	glNormal3fv( &p->nx );
+	glTexCoord2fv( &p->s );
+	glVertex3fv( &p->x );
+}
+
+void
+OsuSphere( float radius, int slices, int stacks )
+{
+	// set the globals:
+
+	NumLngs = slices;
+	NumLats = stacks;
+	if( NumLngs < 3 )
+		NumLngs = 3;
+	if( NumLats < 3 )
+		NumLats = 3;
+
+	// allocate the point data structure:
+
+	Pts = new struct point[ NumLngs * NumLats ];
+
+	// fill the Pts structure:
+
+	for( int ilat = 0; ilat < NumLats; ilat++ )
+	{
+		float lat = -M_PI/2.  +  M_PI * (float)ilat / (float)(NumLats-1);	// ilat=0/lat=0. is the south pole
+											// ilat=NumLats-1, lat=+M_PI/2. is the north pole
+		float xz = cosf( lat );
+		float  y = sinf( lat );
+		for( int ilng = 0; ilng < NumLngs; ilng++ )				// ilng=0, lng=-M_PI and
+											// ilng=NumLngs-1, lng=+M_PI are the same meridian
+		{
+			float lng = -M_PI  +  2. * M_PI * (float)ilng / (float)(NumLngs-1);
+			float x =  xz * cosf( lng );
+			float z = -xz * sinf( lng );
+			struct point* p = PtsPointer( ilat, ilng );
+			p->x  = radius * x;
+			p->y  = radius * y;
+			p->z  = radius * z;
+			p->nx = x;
+			p->ny = y;
+			p->nz = z;
+			p->s = ( lng + M_PI    ) / ( 2.*M_PI );
+			p->t = ( lat + M_PI/2. ) / M_PI;
+		}
+	}
+
+	struct point top, bot;		// top, bottom points
+
+	top.x =  0.;		top.y  = radius;	top.z = 0.;
+	top.nx = 0.;		top.ny = 1.;		top.nz = 0.;
+	top.s  = 0.;		top.t  = 1.;
+
+	bot.x =  0.;		bot.y  = -radius;	bot.z = 0.;
+	bot.nx = 0.;		bot.ny = -1.;		bot.nz = 0.;
+	bot.s  = 0.;		bot.t  =  0.;
+
+	// connect the north pole to the latitude NumLats-2:
+
+	glBegin(GL_TRIANGLE_STRIP);
+	for (int ilng = 0; ilng < NumLngs; ilng++)
+	{
+		float lng = -M_PI + 2. * M_PI * (float)ilng / (float)(NumLngs - 1);
+		top.s = (lng + M_PI) / (2. * M_PI);
+		DrawPoint(&top);
+		struct point* p = PtsPointer(NumLats - 2, ilng);	// ilat=NumLats-1 is the north pole
+		DrawPoint(p);
+	}
+	glEnd();
+
+	// connect the south pole to the latitude 1:
+
+	glBegin( GL_TRIANGLE_STRIP );
+	for (int ilng = NumLngs - 1; ilng >= 0; ilng--)
+	{
+		float lng = -M_PI + 2. * M_PI * (float)ilng / (float)(NumLngs - 1);
+		bot.s = (lng + M_PI) / (2. * M_PI);
+		DrawPoint(&bot);
+		struct point* p = PtsPointer(1, ilng);					// ilat=0 is the south pole
+		DrawPoint(p);
+	}
+	glEnd();
+
+	// connect the horizontal strips:
+
+	for( int ilat = 2; ilat < NumLats-1; ilat++ )
+	{
+		struct point* p;
+		glBegin(GL_TRIANGLE_STRIP);
+		for( int ilng = 0; ilng < NumLngs; ilng++ )
+		{
+			p = PtsPointer( ilat, ilng );
+			DrawPoint( p );
+			p = PtsPointer( ilat-1, ilng );
+			DrawPoint( p );
+		}
+		glEnd();
+	}
+
+	// clean-up:
+
+	delete [ ] Pts;
+	Pts = NULL;
 }
